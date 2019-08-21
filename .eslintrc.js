fb91f5c914d000c71bdf20a475ab7625f17552b8
Dominic Dambrogia <dominic.dambrogia@palmettostatearmory.com>
2019-08-21 15:19:52 -0700

    adjust quote-props property

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     "extends": "airbnb-base",
     "rules": {
         "no-param-reassign": 0,
-        "no-return-assign": 0
+        "no-return-assign": 0,
+        "quote-props": ["error", "consistent"]
     }
 };
