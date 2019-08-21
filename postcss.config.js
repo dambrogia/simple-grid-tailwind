0fb1f7095bf2c34109927d90524e598954fb2df8
Dominic Dambrogia <dominic.dambrogia@palmettostatearmory.com>
2019-08-21 15:16:32 -0700

    eslint

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,12 @@
-const { getScopeFromCli, getMinifyFromCli } = require('./src/util')
-
+const { getScopeFromCli, getMinifyFromCli } = require('./src/util');
+/* eslint-disable */
 module.exports = {
   plugins: [
     require('tailwindcss')('./tailwind.config.js'),
-    require('./src/simpleGrid')({scope: getScopeFromCli()}),
+    require('./src/simpleGrid')({ scope: getScopeFromCli() }),
     require('@csstools/postcss-sass'),
     require('autoprefixer'),
-    getMinifyFromCli() ? require('cssnano')({preset: 'default'}) : () => {}
-  ]
-}
+    getMinifyFromCli() ? require('cssnano')({ preset: 'default' }) : () => {},
+  ],
+};
+/* eslint-enable */
